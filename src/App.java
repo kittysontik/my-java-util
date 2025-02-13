b68d6e11869f548faadba914810de42f3b1bbf1a
Ekaterina Tarkhanova <kat.tarhanova@gmail.com>
2025-02-13 14:46:54 +0700

    decompose method getOpenFiles

--- src/App.java
+++ src/App.java
@@ -7,50 +7,52 @@ import java.util.List;
 
 public class App {
 
+  public static List<BufferedReader> getOpenFiles(String[] filenames) {
+    List<BufferedReader> openedFiles = new ArrayList<>();
+
+    for (String filename : filenames) {
+      try {
+        BufferedReader openedFile = new BufferedReader(
+            new FileReader(filename));
+        openedFiles.add(openedFile);
+
+      } catch (FileNotFoundException e) {
+        System.out.println("Ошибка: файл '" + filename + "' не найден.");
+      }
+    }
+    return openedFiles;
+  }
+
   public static void main(String[] args) {
     if (args.length < 1) {
       System.out.println("Ошибка: укажите хотя бы один файл.");
       return;
     }
-    // Создаем список для всех файлов
-    List<BufferedReader> readers = new ArrayList<>();
-
+    List<BufferedReader> openedFiles = getOpenFiles(args);
     try {
-      for (String filename : args) {
-        try {
-          FileReader fileReader = new FileReader(filename); // Открываем файл
-          BufferedReader bufferedReader = new BufferedReader(
-              fileReader); // Оборачиваем для построчного чтения
-          readers.add(bufferedReader); // Добавляем в список
-
-        } catch (FileNotFoundException e) {
-          System.out.println("Ошибка: файл '" + filename + "' не найден.");
-        }
-      }
-
       boolean filesHaveLines = true;
       while (filesHaveLines) {
         filesHaveLines = false;
-        for (BufferedReader reader : readers) {
-          String line = reader.readLine();
+        for (BufferedReader openedFile : openedFiles) {
+          String line = openedFile.readLine();
           if (line != null) {
             System.out.println(line);
             filesHaveLines = true;
           }
         }
+
       }
     } catch (IOException e) {
       System.out.println("Ошибка при чтении файлов.");
-    } finally {
-      // Закрываем все файлы
-      for (BufferedReader reader : readers) {
-        try {
-          if (reader != null) {
-            reader.close();
-          }
-        } catch (IOException e) {
-          System.out.println("Ошибка при закрытии файла.");
+    }
+    // Закрываем все файлы
+    for (BufferedReader openedFile : openedFiles) {
+      try {
+        if (openedFile != null) {
+          openedFile.close();
         }
+      } catch (IOException e) {
+        System.out.println("Ошибка при закрытии файла.");
       }
     }
   }
