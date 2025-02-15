1d1de89d49d98dc92412a420828dd4b9acc23868
Ekaterina Tarkhanova <kat.tarhanova@gmail.com>
2025-02-15 17:23:09 +0700

    Add writers to file and option -a

--- src/App.java
+++ src/App.java
@@ -1,11 +1,19 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class App {
+
+  private static boolean appendMode = false;
+  private static BufferedWriter intWriter;
+  private static BufferedWriter floatWriter;
+  private static BufferedWriter stringWriter;
+
 
   // входная точка для запуска программы
   public static void main(String[] args) {
@@ -14,14 +22,55 @@ public class App {
       return;
     }
 
-    List<BufferedReader> openedFiles = getOpenFiles(args);
-    fileParser(openedFiles);
-    closeFiles(openedFiles);
+    appendMode = checkAppendOption(args);
+    try {
+      initWriters();
+      List<BufferedReader> openedFiles = getOpenedFiles(args);
+      fileParser(openedFiles);
+      closeFiles(openedFiles);
+    } finally {
+      closeWriters();
+    }
+  }
 
+  private static boolean checkAppendOption(String[] args) {
+    for (String arg : args) {
+      switch (arg) {
+        case "-a":
+          return true;
+      }
+    }
+    return false;
+  }
+
+  public static void initWriters() {
+    try {
+      intWriter = new BufferedWriter(new FileWriter("integers.txt", appendMode));
+      floatWriter = new BufferedWriter(new FileWriter("floats.txt", appendMode));
+      stringWriter = new BufferedWriter(new FileWriter("strings.txt", appendMode));
+    } catch (IOException e) {
+      System.out.println("Ошибка при создании файлов для записи.");
+    }
+  }
+
+  public static void closeWriters() {
+    try {
+      if (intWriter != null) {
+        intWriter.close();
+      }
+      if (floatWriter != null) {
+        floatWriter.close();
+      }
+      if (stringWriter != null) {
+        stringWriter.close();
+      }
+    } catch (IOException e) {
+      System.out.println("Ошибка при закрытии файлов.");
+    }
   }
 
   // метод, к-ый получает открытые файлы
-  public static List<BufferedReader> getOpenFiles(String[] filenames) {
+  public static List<BufferedReader> getOpenedFiles(String[] filenames) {
     List<BufferedReader> openedFiles = new ArrayList<>();
 
     for (String filename : filenames) {
@@ -87,30 +136,48 @@ public class App {
   }
 
   public static void fileParser(List<BufferedReader> openedFiles) {
-    try {
-      boolean filesHaveLines = true;
-      while (filesHaveLines) {
-        filesHaveLines = false;
-        for (BufferedReader openedFile : openedFiles) {
+    boolean filesHaveLines = true;
+    while (filesHaveLines) {
+      filesHaveLines = false;
+      for (BufferedReader openedFile : openedFiles) {
+        try {
           String line = openedFile.readLine();
           if (line != null) {
             if (isInteger(line)) {
-              System.out.println("Целое число: " + line);
+              writeToFile(intWriter, line);
             } else if (isFloat(line)) {
-              System.out.println("Дробное число: " + line);
+              writeToFile(floatWriter, line);
             } else {
-              System.out.println("Строка: " + line);
+              writeToFile(stringWriter, line);
             }
             filesHaveLines = true;
           }
+        } catch (IOException e) {
+          System.out.println("Ошибка при чтении файлов.");
         }
       }
-    } catch (IOException e) {
-      System.out.println("Ошибка при чтении файлов.");
     }
   }
 
+  public static void writeToFile(BufferedWriter writer, String line) {
+    try {
+      writer.write(line);
+      writer.newLine();
 
+    } catch (IOException e) {
+      System.out.println("Ошибка при записи строки: " + line);
+    }
+  }
+
+  public static void clearFiles(String filename) {
+    try {
+      new FileWriter(filename, false).close();
+      new FileWriter(filename, false).close();
+      new FileWriter(filename, false).close();
+    } catch (IOException e) {
+      System.out.println("Ошибка при очистке файлов.");
+    }
+  }
 }
 
 
